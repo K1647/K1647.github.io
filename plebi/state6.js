01b5fb9462890b95898e728a4e650f2a6f78b45d
K1647 <K1647@student.jamk.fi>
2018-01-30 03:42:17 +0200

    state3 is rdy to roll

---
+++ plebi/state6.js
@@ -0,0 +1,9 @@
+demo.state6 = function(){};
+demo.state6.prototype = {
+    preload: function(){},
+    create: function(){
+        game.stage.backgroundColor = '#a6ff4d';
+        addChangeStateEventListeners();
+    },
+    update: function(){}
+};
